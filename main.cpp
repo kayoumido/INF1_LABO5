e5375ff1e4db60d4e2ccc66b515702f5cfe2bfb0
Doran Kayoumi <dorankayoumi@gmail.com>
2018-11-14 11:43:41 +0100

    Add file header

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+/* ---------------------------
+ Laboratoire : 5
+ Fichier : main.cpp
+ Auteur(s) :
+ Date : 14.11.2018
+ But :
+
+ Remarque(s) :
+ Compilateur : g++ 6.3.0
+ --------------------------- */
+
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  cout << "Hello, World!" << endl;
   return 0;
 }
