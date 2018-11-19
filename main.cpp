f7da5803d84ffc8f28cb81fdf40d0fa4e09f8358
Doran Kayoumi <dorankayoumi@gmail.com>
2018-11-19 21:28:43 +0100

    First day of the month placed based on monday position and code refactor

--- main.cpp
+++ main.cpp
@@ -14,67 +14,120 @@
 
 using namespace std;
 
-const int CALENDAR_WIDTH = 21;
-const int NUMBER_DAYS_IN_WEEK = 7;
-const int SEPARATOR_SIZE = 2;
+const unsigned CALENDAR_WIDTH = 21;
+const unsigned NUMBER_DAYS_IN_WEEK = 7;
+const unsigned SEPARATOR_SIZE = 2;
 const char DISPLAY_FILL = '.';
 
-void displayCalendar();
-string getDayName(int day);
+/**
+ * Display the calendar for a given month
+ *
+ * @return void
+ */
+void displayCalendar(int month, int firstDayOfMonth, int mondayPosition);
+
+/**
+ * Get the initial of a day based on it's number
+ *  e.g. 2 (Mardi) --> M
+ *
+ * @param day int the day number (0-6)
+ * @return string day
+ */
+string getDayInitial(int day);
+
+/**
+ * Get the name of a month based on it's number
+ *  e.g 8 --> Aout
+ *
+ * @param month int Month number
+ * @return string month name
+ */
+string getMonthName(int month);
+
+/**
+  * Ask and validates user data
+  *
+  * @param question string question to ask the user
+  * @param errorMessage string error message to display if the user enters non valid value
+  * @param max int max value for the expected value
+  * @param min int min value for the expected value
+  * @return int Validated user input
+  */
+int getUserInput(string question, string errorMessage, int max, int min);
+
+/**
+ * Displays an empty line
+ *
+ * @return void
+ */
+void displayEmptyLine(unsigned colWidth);
+
+/**
+ *
+ * @param text string
+ */
+void displayCenteredText(string text, unsigned colWidth);
 
 int main() {
-  displayCalendar();
-
+  cout << setfill(DISPLAY_FILL);
+  // displayCalendar();
+  displayCalendar(1, 2, 1);
   return 0;
 }
 
-void displayCalendar() {
+void displayCalendar(int month, int firstDayOfMonth, int mondayPosition) {
   string year = "2016";
-  string name = "Mars";
+  string name = getMonthName(month);
   int nbDays = 31;
-  int mondayPosition = 1;
-  int firstDayOfMonth = 3;
 
-  cout << setfill(DISPLAY_FILL);
-  cout << setw(CALENDAR_WIDTH / 2 - (int)year.length() / 2) << DISPLAY_FILL;
-  cout << setw((int)year.length()) << year;
-  cout << setw(CALENDAR_WIDTH - (CALENDAR_WIDTH / 2 - (int)year.length() / 2) - (int)year.length()) << DISPLAY_FILL << endl;
+  displayCenteredText(year, CALENDAR_WIDTH);
+  displayEmptyLine(CALENDAR_WIDTH);
+  displayCenteredText(name, CALENDAR_WIDTH);
 
-  cout << setw(CALENDAR_WIDTH) << DISPLAY_FILL << endl;
-
-  cout << setw(CALENDAR_WIDTH / 2 - (int)name.length() / 2) << DISPLAY_FILL;
-  cout << setw((int)name.length()) <<  name;
-  cout << setw(CALENDAR_WIDTH - (CALENDAR_WIDTH / 2 - (int)name.length() / 2) - (int)name.length()) << DISPLAY_FILL << endl;
-
-  int firstWeekDay = NUMBER_DAYS_IN_WEEK - (mondayPosition - 1);
-  int i = firstWeekDay;
+  int firstDayOfWeek = NUMBER_DAYS_IN_WEEK - (mondayPosition - 1);
+  int i = firstDayOfWeek;
   do {
     if (i == NUMBER_DAYS_IN_WEEK) i = 0;
 
-    cout << setw(3) << getDayName(i);
+    cout << setw(3) << getDayInitial(i);
     i++;
 
-    if (i > 6 and firstWeekDay != NUMBER_DAYS_IN_WEEK) i = 0;
+    if (i > 6 and firstDayOfWeek != NUMBER_DAYS_IN_WEEK) i = 0;
 
-  } while(i != firstWeekDay);
+  } while(i != firstDayOfWeek);
   cout << endl;
+
   
-  for (int j = 1; j <= nbDays; ++j) {
-    int dayPosition = j % NUMBER_DAYS_IN_WEEK;
+  int firstDayMondayPositionDiff = (firstDayOfMonth - 1) + (mondayPosition - 1);
+  int firstDayMonthPosition = firstDayMondayPositionDiff > NUMBER_DAYS_IN_WEEK - 1 ?
+          firstDayMondayPositionDiff - NUMBER_DAYS_IN_WEEK
+          : firstDayMondayPositionDiff;
+
+  for (int p = 0; p < firstDayMonthPosition; ++p) {
+    cout << setw(3) << DISPLAY_FILL;
+  }
+
+  int dayMonthPosition = firstDayMonthPosition;
+  for (int date = 1; date <= nbDays; ++date) {
+    // If we are at the end of the week, pass to the next line, i.e. the next week.
+    if (dayMonthPosition == NUMBER_DAYS_IN_WEEK) {
+      cout << endl;
+      dayMonthPosition = 0;
+    }
 
     cout << DISPLAY_FILL;
-    cout << setw(SEPARATOR_SIZE) << j;
+    cout << setw(SEPARATOR_SIZE) << date;
 
-    // If we are at the end if the week, pass to the next line
-    if (dayPosition == 0) cout << endl;
+    // If it's the last day of the month and it's not a Sunday, fill the end of the line with DISPLAY_FILL
+    if (date == nbDays and dayMonthPosition != NUMBER_DAYS_IN_WEEK - 1) {
+      cout << setw(CALENDAR_WIDTH - (dayMonthPosition * SEPARATOR_SIZE + dayMonthPosition + 3 )) << DISPLAY_FILL;
+    }
 
-    // If it's the last day of the month, fill the end of the line with DISPLAY_FILL
-    if (j == nbDays and j % NUMBER_DAYS_IN_WEEK != 0)
-      cout << setw(CALENDAR_WIDTH - (dayPosition * SEPARATOR_SIZE + dayPosition)) << DISPLAY_FILL;
+    ++dayMonthPosition;
   }
 }
 
-string getDayName(int day) {
+string getDayInitial(int day) {
   switch (day) {
     case 0:
       return "L";
@@ -90,5 +143,44 @@ string getDayName(int day) {
     case 6:
       return "D";
   }
+}
+
+string getMonthName(int month) {
+  switch (month) {
+    case 0:
+      return "Janvier";
+    case 1:
+      return "Fevrier";
+    case 2:
+      return "Mars";
+    case 3:
+      return "Avril";
+    case 4:
+      return "Mai";
+    case 5:
+      return "Juin";
+    case 6:
+      return "Juillet";
+    case 7:
+      return "Aout";
+    case 8:
+      return "Septembre";
+    case 9:
+      return "Octobre";
+    case 10:
+      return "Novembre";
+    case 11:
+      return "Decembre";
+  }
+}
+
+void displayCenteredText(const string text, const unsigned colWidth) {
+  cout << setw(colWidth / 2 - (int)text.length() / 2) << DISPLAY_FILL;
+  cout << setw((int)text.length()) << text;
+  cout << setw(colWidth - (colWidth / 2 - (int)text.length() / 2) - (int)text.length()) << DISPLAY_FILL << endl;
+}
+
+void displayEmptyLine(const unsigned colWidth) {
+  cout << setw(colWidth) << DISPLAY_FILL << endl;
 }
 
